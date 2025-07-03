b1c69f1a58419c22b1659cdfc9132ef2de8d0888
oliverkean <oliverpalgue@gmail.com>
2025-07-04 04:44:51 +0800

    feat: test

--- config/db.php
+++ config/db.php
@@ -1,16 +1,17 @@
 <?php
 session_start();
+
 $host = $_ENV['MYSQLHOST'] ?? 'localhost';
-$username = $_ENV['MYSQLHOST'] ?? 'root';
-$password = $_ENV['MYSQLHOST'] ?? '';
-$dbname = $_ENV['MYSQLHOST'] ?? 'php_test';
-$port = $_ENV['MYSQLHOST'] ?? '3306';
+$username = $_ENV['MYSQLUSER'] ?? 'root';
+$password = $_ENV['MYSQLPASSWORD'] ?? '';
+$dbname = $_ENV['MYSQLDATABASE'] ?? 'php_test';
+$port = $_ENV['MYSQLPORT'] ?? '3306';
 
 try {
-    $connect = new PDO("mysql:host=" . host . "; db=" . db, user, pass);
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+    $connect = new PDO($dsn, $username, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-    echo $e->getMessage();
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
